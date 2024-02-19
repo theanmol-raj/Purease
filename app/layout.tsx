1ddb1f6a955ca3307eba778f85cfd80b16fc6fa9
Anmol Raj <course.anmolraj@gmail.com>
2024-02-19 20:03:12 +0530

    added  file structure and typings file

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className=" h-40 w-40 bg-red-600" />
+        {children}</body>
     </html>
   );
 }
