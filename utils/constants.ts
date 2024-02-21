e1802d920bf9c34c1e69f7062ca72e3b8b22ad72
Anmol Raj <course.anmolraj@gmail.com>
2024-02-21 08:49:47 +0530

    removed conflicting types

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,12 +1,2 @@
 
 
-type Coreoffering = { icon: any; offeringName: string };
-
-const coreofferings: Readonly<Coreoffering[]> = [
-  { icon: <CheckCircle2 />, offeringName: "jj" },
-  { icon: <CheckCircle2 />, offeringName: "jj" },
-  { icon: <CheckCircle2 />, offeringName: "jj" },
-  { icon: <CheckCircle2 />, offeringName: "jj" },
-  { icon: <CheckCircle2 />, offeringName: "jj" },
-  { icon: <CheckCircle2 />, offeringName: "jj" },
-] as const;
